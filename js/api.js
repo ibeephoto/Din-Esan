9bb75bc976978af500f604d5674be3d36c28b022
ibeephoto <ibeephoto277@gmail.com>
2026-06-29 17:22:35 +0700

    Delete js directory

--- js/api.js
+++
@@ -1,9 +0,0 @@
-import { enqueueSync } from './syncQueue.js';
-
-export function addRecord(record) {
-  enqueueSync('UPSERT_RECORD', record);
-}
-
-export function deleteRecord(id) {
-  enqueueSync('DELETE_RECORD', { id });
-}
--- js/state.js
+++
@@ -1,6 +0,0 @@
-export const state = {
-  records: [],
-  appointments: [],
-  queue: [],
-  isOnline: true
-};
